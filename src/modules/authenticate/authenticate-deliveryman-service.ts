e446fef183a4e8873648bf006b98a6ee369b8242
Newton Duarte <newton_@hotmail.com>
2022-10-26 07:13:51 -0300

    feat: add authenticate deliveryman service

---
+++ src/modules/authenticate/authenticate-deliveryman-service.ts
@@ -0,0 +1,39 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { jwt } from "../../config/jwt";
+import { prisma } from "../../database/prisma";
+
+interface IAuthenticateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateDeliverymanService {
+  async execute({ username, password }: IAuthenticateDeliveryman) {
+    const deliveryman = await prisma.deliveryman.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: 'insensitive'
+        }
+      }
+    });
+
+    if (!deliveryman) {
+      throw new Error('Invalid credentials!');
+    }
+
+    const passwordMatches = await compare(password, deliveryman.password);
+
+    if (!passwordMatches) {
+      throw new Error('Invalid credentials!');
+    }
+
+    const token = await sign({ username }, String(jwt.secret), {
+      subject: username,
+      expiresIn: '1d'
+    });
+
+    return token;
+  }
+}
