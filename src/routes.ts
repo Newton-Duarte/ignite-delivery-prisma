6f6d526a3ed448c7c580c839df96069d14fbc946
Newton Duarte <newton_@hotmail.com>
2022-10-26 07:03:17 -0300

    feat: add authenticate route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { AuthenticateController } from './modules/authenticate/authenticate-controller';
 import { ClientController } from './modules/clients/controllers/client-controller';
 
 const routes = Router();
 
 const clientController = new ClientController();
+const authenticateController = new AuthenticateController();
 
+routes.post('/authenticate', authenticateController.create);
 routes.post('/clients', clientController.create);
 
 export { routes };
