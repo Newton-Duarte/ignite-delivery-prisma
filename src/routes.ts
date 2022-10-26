b4e86df49ce6fb12338bbf12f49ecf09c4d95b0b
Newton Duarte <newton_@hotmail.com>
2022-10-26 07:09:35 -0300

    feat: add deliverymen routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { AuthenticateController } from './modules/authenticate/authenticate-controller';
 import { ClientController } from './modules/clients/controllers/client-controller';
+import { DeliverymanController } from './modules/deliveryman/controllers/deliveryman-controller';
 
 const routes = Router();
 
-const clientController = new ClientController();
 const authenticateController = new AuthenticateController();
+const clientController = new ClientController();
+const deliverymanController = new DeliverymanController();
 
 routes.post('/authenticate', authenticateController.create);
 routes.post('/clients', clientController.create);
+routes.post('/deliverymen', deliverymanController.create);
 
 export { routes };
