ffc231aea5dea482cae2e9ac4c33de98c0fe6bba
Newton Duarte <newton_@hotmail.com>
2022-10-28 06:47:05 -0300

    feat: add ensure client authenticated express middleware

---
+++ src/middlewares/ensureClientAuthenticated.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { jwt } from "../config/jwt";
+
+type IPayload = {
+  sub: string;
+};
+
+export function ensureClientAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return response.status(401).json({ message: 'Missing token' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub } = verify(token, String(jwt.secret)) as IPayload;
+    request.client_id = sub;
+    console.log(sub);
+    return next();
+  } catch (error) {
+    return response.status(401).json({ message: 'Invalid token' });
+  }
+}
