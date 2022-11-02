32f1fa9823ee55aed1bfcee080632e9c572328f8
Newton Duarte <newton_@hotmail.com>
2022-11-02 06:47:04 -0300

    feat: add ensure deliveryman authenticated middleware

---
+++ src/middlewares/ensureDeliverymanAuthenticated.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { jwt } from "../config/jwt";
+
+type IPayload = {
+  sub: string;
+};
+
+export function ensureDeliverymanAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return response.status(401).json({ message: 'Missing token' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub } = verify(token, String(jwt.secret_deliveryman)) as IPayload;
+    request.deliveryman_id = sub;
+    return next();
+  } catch (error) {
+    return response.status(401).json({ message: 'Invalid token' });
+  }
+}
