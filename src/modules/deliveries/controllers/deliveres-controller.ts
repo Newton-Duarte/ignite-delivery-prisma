40768ebb7585589a4ac125d2ede4faa0a07dd9f0
Newton Duarte <newton_@hotmail.com>
2022-11-02 06:47:47 -0300

    feat: add list deliveries controller method

--- src/modules/deliveries/controllers/deliveres-controller.ts
+++ src/modules/deliveries/controllers/deliveres-controller.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from "express";
 import { CreateDeliveryService } from "../services/create-delivery-service";
+import { FindPendingDeliveries } from "../services/find-pending-deliveries";
+import { UpdateDeliveryService } from "../services/update-delivery-service";
 
 export class DeliveriesController {
+  async index(request: Request, response: Response) {
+    const findPendingDeliveriesService = new FindPendingDeliveries();
+    const pendingDeliveries = await findPendingDeliveriesService.execute();
+    return response.json(pendingDeliveries);
+  }
+
   async create(request: Request, response: Response) {
     const { item_name } = request.body;
     const { client_id } = request;
