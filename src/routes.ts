a35c6fdb164f160f051ab9822901cba17f9f331e
Newton Duarte <newton_@hotmail.com>
2022-11-02 06:56:03 -0300

    feat: add clients deliveries route

--- src/routes.ts
+++ src/routes.ts
@@ -6,19 +6,24 @@ import { ClientController } from './modules/clients/controllers/client-controlle
 import { ensureClientAuthenticated } from './middlewares/ensureClientAuthenticated';
 import { DeliveriesController } from './modules/deliveries/controllers/deliveries-controller';
 import { ensureDeliverymanAuthenticated } from './middlewares/ensureDeliverymanAuthenticated';
+import { ClientDeliveriesController } from './modules/clients/controllers/client-deliveries-controller';
 
 const routes = Router();
 
 const authenticateClientController = new AuthenticateClientController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 const clientController = new ClientController();
+const clientDeliveriesController = new ClientDeliveriesController();
 const deliverymanController = new DeliverymanController();
 const deliveriesController = new DeliveriesController();
 
+routes.get('/clients/deliveries', ensureClientAuthenticated, clientDeliveriesController.index);
 routes.post('/clients', clientController.create);
 routes.post('/clients/authenticate', authenticateClientController.create);
+
 routes.post('/deliverymen', deliverymanController.create);
 routes.post('/deliverymen/authenticate', authenticateDeliverymanController.create);
+
 routes.get('/deliveries', ensureDeliverymanAuthenticated, deliveriesController.index);
 routes.post('/deliveries', ensureClientAuthenticated, deliveriesController.create);
 routes.put('/deliveries/:delivery_id', ensureDeliverymanAuthenticated, deliveriesController.update);
