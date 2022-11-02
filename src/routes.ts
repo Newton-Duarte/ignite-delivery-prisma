8c2214f898974a61b6e673e608be561b6fdb78aa
Newton Duarte <newton_@hotmail.com>
2022-11-02 07:21:13 -0300

    feat: add finalize deliveries route

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ import { DeliveriesController } from './modules/deliveries/controllers/deliverie
 import { ensureDeliverymanAuthenticated } from './middlewares/ensureDeliverymanAuthenticated';
 import { ClientDeliveriesController } from './modules/clients/controllers/client-deliveries-controller';
 import { DeliverymanDeliveriesController } from './modules/deliveryman/controllers/deliveryman-deliveries-controller';
+import { FinalizeDeliveryController } from './modules/deliveries/controllers/finalize-delivery-controller';
 
 const routes = Router();
 
@@ -18,6 +19,7 @@ const clientDeliveriesController = new ClientDeliveriesController();
 const deliverymanController = new DeliverymanController();
 const deliverymanDeliveriesController = new DeliverymanDeliveriesController();
 const deliveriesController = new DeliveriesController();
+const finalizeDeliveriesController = new FinalizeDeliveryController();
 
 routes.get('/clients/deliveries', ensureClientAuthenticated, clientDeliveriesController.index);
 routes.post('/clients', clientController.create);
@@ -30,5 +32,6 @@ routes.post('/deliverymen/authenticate', authenticateDeliverymanController.creat
 routes.get('/deliveries', ensureDeliverymanAuthenticated, deliveriesController.index);
 routes.post('/deliveries', ensureClientAuthenticated, deliveriesController.create);
 routes.put('/deliveries/:delivery_id', ensureDeliverymanAuthenticated, deliveriesController.update);
+routes.patch('/deliveries/:delivery_id', ensureDeliverymanAuthenticated, finalizeDeliveriesController.update);
 
 export { routes };
