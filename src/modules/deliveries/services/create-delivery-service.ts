b5101f4da0b78b432b5d2351173f3f7106ed7331
Newton Duarte <newton_@hotmail.com>
2022-11-16 06:45:39 -0300

    feat: add address field to create delivery service

--- src/modules/deliveries/services/create-delivery-service.ts
+++ src/modules/deliveries/services/create-delivery-service.ts
@@ -3,14 +3,16 @@ import { prisma } from "../../../database/prisma";
 interface ICreateDelivery {
   item_name: string;
   client_id: string;
+  address: string;
 }
 
 export class CreateDeliveryService {
-  async execute({ item_name, client_id }: ICreateDelivery) {
+  async execute({ item_name, client_id, address }: ICreateDelivery) {
     const delivery = await prisma.deliveries.create({
       data: {
         item_name,
-        client_id
+        client_id,
+        address
       }
     });
 
