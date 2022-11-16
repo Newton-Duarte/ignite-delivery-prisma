684ebfd754b471833a97d5dda34c6502a9587b23
Newton Duarte <newton_@hotmail.com>
2022-11-16 06:44:48 -0300

    feat: add cors config

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import { routes } from './routes';
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
