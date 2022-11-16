7d37bf5d9b35dd2985441d0da3ac1f05ffdc5ce0
Newton Duarte <newton_@hotmail.com>
2022-11-16 06:45:17 -0300

    feat: add address field to create client method

--- src/modules/clients/controllers/client-controller.ts
+++ src/modules/clients/controllers/client-controller.ts
@@ -3,11 +3,11 @@ import { CreateClientService } from "../services/create-client-service";
 
 export class ClientController {
   async create(request: Request, response: Response) {
-    const { username, password } = request.body;
+    const { username, password, address } = request.body;
 
     const createClientService = new CreateClientService();
 
-    const client = await createClientService.execute({ username, password });
+    const client = await createClientService.execute({ username, password, address });
 
     return response.json(client);
   }
