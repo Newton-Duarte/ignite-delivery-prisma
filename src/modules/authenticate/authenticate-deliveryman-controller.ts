bd0f900c7fcbea20d4a22b8475fcd2cebd239780
Newton Duarte <newton_@hotmail.com>
2022-11-17 05:55:38 -0300

    feat: add result object with user and token to authenticate deliveryman method

--- src/modules/authenticate/authenticate-deliveryman-controller.ts
+++ src/modules/authenticate/authenticate-deliveryman-controller.ts
@@ -7,8 +7,8 @@ export class AuthenticateDeliverymanController {
 
     const authenticateService = new AuthenticateDeliverymanService();
 
-    const token = await authenticateService.execute({ username, password });
+    const result = await authenticateService.execute({ username, password });
 
-    return response.json({ token });
+    return response.json(result);
   }
 }
