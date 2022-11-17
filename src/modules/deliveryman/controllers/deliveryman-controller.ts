09d87909ba71eaff749c8bdef657fa6c19e351dc
Newton Duarte <newton_@hotmail.com>
2022-11-17 05:53:23 -0300

    feat: get name from create deliveryman request

--- src/modules/deliveryman/controllers/deliveryman-controller.ts
+++ src/modules/deliveryman/controllers/deliveryman-controller.ts
@@ -3,11 +3,11 @@ import { CreateDeliverymanService } from "../services/create-deliveryman-service
 
 export class DeliverymanController {
   async create(request: Request, response: Response) {
-    const { username, password } = request.body;
+    const { name, username, password } = request.body;
 
     const createDeliverymanService = new CreateDeliverymanService();
 
-    const deliveryman = await createDeliverymanService.execute({ username, password });
+    const deliveryman = await createDeliverymanService.execute({ name, username, password });
 
     return response.json(deliveryman);
   }
