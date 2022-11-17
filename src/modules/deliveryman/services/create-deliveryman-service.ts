a6b6ced8554efe93dc42938deaf3421d18e0d4d9
Newton Duarte <newton_@hotmail.com>
2022-11-17 05:53:47 -0300

    feat: add name to create deliveryman service

--- src/modules/deliveryman/services/create-deliveryman-service.ts
+++ src/modules/deliveryman/services/create-deliveryman-service.ts
@@ -2,12 +2,13 @@ import { hash } from "bcrypt";
 import { prisma } from "../../../database/prisma";
 
 interface ICreateDeliveryman {
+  name: string;
   username: string;
   password: string;
 }
 
 export class CreateDeliverymanService {
-  async execute({ username, password }: ICreateDeliveryman) {
+  async execute({ name, username, password }: ICreateDeliveryman) {
     const deliverymanAlreadyExists = await prisma.deliveryman.findFirst({
       where: {
         username: {
@@ -25,6 +26,7 @@ export class CreateDeliverymanService {
 
     const deliveryman = await prisma.deliveryman.create({
       data: {
+        name,
         username,
         password: hashPassword,
       }
