f861bfdccad2a97d4c31244ce69a537cfe2fc3d8
Newton Duarte <newton_@hotmail.com>
2022-11-18 06:31:52 -0300

    feat: add index method to deliveryman controller

--- src/modules/deliveryman/controllers/deliveryman-controller.ts
+++ src/modules/deliveryman/controllers/deliveryman-controller.ts
@@ -1,7 +1,18 @@
 import { json, Request, Response } from "express";
 import { CreateDeliverymanService } from "../services/create-deliveryman-service";
+import { FindDeliverymanService } from "../services/find-deliveryman-service";
 
 export class DeliverymanController {
+  async index(request: Request, response: Response) {
+    const deliveryman_id = request.deliveryman_id;
+
+    const findDeliverymanService = new FindDeliverymanService();
+
+    const client = await findDeliverymanService.execute(deliveryman_id);
+
+    return response.json(client);
+  }
+
   async create(request: Request, response: Response) {
     const { name, username, password } = request.body;
 
