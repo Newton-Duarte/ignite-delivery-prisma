6aa7ddda45944144d4327b4bc8b9c1b6679e7725
Newton Duarte <newton_@hotmail.com>
2022-11-18 06:31:45 -0300

    feat: add index method to client controller

--- src/modules/clients/controllers/client-controller.ts
+++ src/modules/clients/controllers/client-controller.ts
@@ -1,7 +1,18 @@
 import { json, Request, Response } from "express";
 import { CreateClientService } from "../services/create-client-service";
+import { FindClientService } from "../services/find-client-service";
 
 export class ClientController {
+  async index(request: Request, response: Response) {
+    const client_id = request.client_id;
+
+    const findClientService = new FindClientService();
+
+    const client = await findClientService.execute(client_id);
+
+    return response.json(client);
+  }
+
   async create(request: Request, response: Response) {
     const { name, username, password, address } = request.body;
 
