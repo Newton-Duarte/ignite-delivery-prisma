92196434694e9515e36466e20767020c43b78c66
Newton Duarte <newton_@hotmail.com>
2022-11-22 06:28:57 -0300

    fix: find deliveryman deliveries service

--- src/modules/deliveryman/services/find-deliveryman-deliveries-service.ts
+++ src/modules/deliveryman/services/find-deliveryman-deliveries-service.ts
@@ -2,15 +2,10 @@ import { prisma } from "../../../database/prisma";
 
 export class FindDeliverymanDeliveriesService {
   async execute(deliveryman_id: string) {
-    const deliveries = await prisma.deliveryman.findMany({
+    const deliveries = await prisma.deliveries.findMany({
       where: {
-        id: deliveryman_id
+        deliveryman_id
       },
-      select: {
-        id: true,
-        username: true,
-        deliveries: true,
-      }
     });
 
     return deliveries;
