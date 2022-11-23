75567f510c1fbe699d4d9cb4c90cebe7607e48e3
Newton Duarte <newton_@hotmail.com>
2022-11-23 07:05:33 -0300

    feat: include client and deliveryman data on deliveryman deliveries list

--- src/modules/deliveryman/services/find-deliveryman-deliveries-service.ts
+++ src/modules/deliveryman/services/find-deliveryman-deliveries-service.ts
@@ -6,6 +6,10 @@ export class FindDeliverymanDeliveriesService {
       where: {
         deliveryman_id
       },
+      include: {
+        client: true,
+        deliveryman: true
+      }
     });
 
     return deliveries;
