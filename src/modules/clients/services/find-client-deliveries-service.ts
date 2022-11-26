264fcdac9887644e9ce1f74f80343a4b1e5e4da5
Newton Duarte <newton_@hotmail.com>
2022-11-26 11:27:54 -0300

    feat: add pagination to client deliveries

--- src/modules/clients/services/find-client-deliveries-service.ts
+++ src/modules/clients/services/find-client-deliveries-service.ts
@@ -1,7 +1,13 @@
 import { prisma } from "../../../database/prisma";
 
+interface IRequest {
+  client_id: string
+  page: number
+  per_page?: number
+}
+
 export class FindClientDeliveriesService {
-  async execute(client_id: string) {
+  async execute({ client_id, page, per_page = 5 }: IRequest) {
     const totalDeliveries = await prisma.deliveries.count({
       where: {
         client_id
@@ -11,6 +17,8 @@ export class FindClientDeliveriesService {
       where: {
         client_id
       },
+      skip: (page - 1) * per_page,
+      take: per_page,
       include: {
         client: true,
         deliveryman: true
