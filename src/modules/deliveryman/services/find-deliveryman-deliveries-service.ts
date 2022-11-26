5c414f0209a7a1abe3e612d91d9e94b65c916cdb
Newton Duarte <newton_@hotmail.com>
2022-11-26 11:29:55 -0300

    feat: add pagination to deliveryman deliveries

--- src/modules/deliveryman/services/find-deliveryman-deliveries-service.ts
+++ src/modules/deliveryman/services/find-deliveryman-deliveries-service.ts
@@ -1,11 +1,18 @@
 import { prisma } from "../../../database/prisma";
 
+interface IRequest {
+  deliveryman_id: string
+  page: number
+  per_page?: number
+}
 export class FindDeliverymanDeliveriesService {
-  async execute(deliveryman_id: string) {
+  async execute({ deliveryman_id, page, per_page = 5 }: IRequest) {
     const deliveries = await prisma.deliveries.findMany({
       where: {
         deliveryman_id
       },
+      skip: (page - 1) * per_page,
+      take: per_page,
       include: {
         client: true,
         deliveryman: true
