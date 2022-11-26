7b7c3bd25f38ff41615d92a204ef1c910a6d6814
Newton Duarte <newton_@hotmail.com>
2022-11-26 11:53:56 -0300

    feat: add per page to client deliveries index method

--- src/modules/clients/controllers/client-deliveries-controller.ts
+++ src/modules/clients/controllers/client-deliveries-controller.ts
@@ -3,11 +3,11 @@ import { FindClientDeliveriesService } from "../services/find-client-deliveries-
 
 export class ClientDeliveriesController {
   async index(request: Request, response: Response) {
-    const { page } = request.query;
+    const { page, per_page } = request.query;
     const { client_id } = request;
 
     const findClientDeliveriesService = new FindClientDeliveriesService();
-    const clientDeliveries = await findClientDeliveriesService.execute({ client_id, page: Number(page) || 1 });
+    const clientDeliveries = await findClientDeliveriesService.execute({ client_id, page: Number(page) || 1, per_page: Number(per_page) || 5 });
 
     return response.json(clientDeliveries);
   }
