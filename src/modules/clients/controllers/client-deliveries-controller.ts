a68c875004dcf4314ad0c29a5afa8e5f4d038a62
Newton Duarte <newton_@hotmail.com>
2022-11-29 06:37:55 -0300

    feat: add search, sort and sort by to client deliveries

--- src/modules/clients/controllers/client-deliveries-controller.ts
+++ src/modules/clients/controllers/client-deliveries-controller.ts
@@ -3,11 +3,19 @@ import { FindClientDeliveriesService } from "../services/find-client-deliveries-
 
 export class ClientDeliveriesController {
   async index(request: Request, response: Response) {
-    const { page, per_page } = request.query;
+    const { page, per_page, search, sort_by, sort } = request.query;
     const { client_id } = request;
 
     const findClientDeliveriesService = new FindClientDeliveriesService();
-    const clientDeliveries = await findClientDeliveriesService.execute({ client_id, page: Number(page) || 1, per_page: Number(per_page) || 5 });
+    const clientDeliveries = await findClientDeliveriesService
+      .execute({
+        client_id,
+        page: Number(page) || 1,
+        per_page: Number(per_page) || 5,
+        sort: sort ? String(sort) : 'desc',
+        sort_by: sort_by ? String(sort_by) : 'created_at',
+        search: search ? String(search) : ''
+      });
 
     return response.json(clientDeliveries);
   }
