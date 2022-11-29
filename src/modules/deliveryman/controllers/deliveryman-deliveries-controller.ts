f717fa89cc4f2ca98d4aa4330e033658c509690b
Newton Duarte <newton_@hotmail.com>
2022-11-29 06:38:02 -0300

    feat: add search, sort and sort by to client deliveries

--- src/modules/deliveryman/controllers/deliveryman-deliveries-controller.ts
+++ src/modules/deliveryman/controllers/deliveryman-deliveries-controller.ts
@@ -3,11 +3,19 @@ import { FindDeliverymanDeliveriesService } from "../services/find-deliveryman-d
 
 export class DeliverymanDeliveriesController {
   async index(request: Request, response: Response) {
-    const { page, per_page } = request.query;
+    const { page, per_page, search, sort_by, sort } = request.query;
     const { deliveryman_id } = request;
 
     const findDeliverymanDeliveriesService = new FindDeliverymanDeliveriesService();
-    const deliverymanDeliveries = await findDeliverymanDeliveriesService.execute({ deliveryman_id, page: Number(page) || 1, per_page: Number(per_page) || 5 });
+    const deliverymanDeliveries = await findDeliverymanDeliveriesService
+      .execute({
+        deliveryman_id,
+        page: Number(page) || 1,
+        per_page: Number(per_page) || 5,
+        sort: String(sort) || 'desc',
+        sort_by: String(sort_by) || 'created_at',
+        search: search ? String(search) : ''
+      });
 
     return response.json(deliverymanDeliveries);
   }
